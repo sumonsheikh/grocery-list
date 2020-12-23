c6086f7b21b82d8addfde86791aec5a88e7b86ea
sumonsheikh <sm.sumon117@gmail.com>
2020-12-23 18:25:17 +0600

    create alert and list component and also implement the app component structur

--- src/App.js
+++ src/App.js
@@ -3,8 +3,37 @@ import Alert from './component/Alert';
 import List from './component/List';
 
 function App() {
+  const [name, setName]           = useState('');
+  const [list, setList]           = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEditId]       = useState(null);
+  const [alert, setAlert]         = useState({show:false, msg:'', type:''});
+  //handle form submit
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log('hello');
+  }
   return (
-    <h2>Appp</h2>
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert/>}
+        <h3>Grocery List</h3>
+        <div className="form-control">
+          <input 
+            type="text" 
+            placeholder="e.g. : egg" 
+            value={name} 
+            onChange={(e)=>setName(e.target.value)}
+            className="grocery"
+          />
+          <button type="submit" className="btn-submit">{isEditing?'edit':'submit'}</button>
+        </div>
+      </form>
+      <div className="grocery-container">
+        <List/>
+      </div>
+      <button className="btn-clear">clear items</button>
+    </section>
   );
 }
 
--- src/component/List.js
+++ src/component/List.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {FaEdit, FaTrash} from 'react-icons/fa';
 
  const List = () => {
     return (
